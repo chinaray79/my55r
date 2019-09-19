db84cd2bad65ead01d00db540fb761d30cbcb120
chinaray79 <rayjohnson79@gmail.com>
2019-09-19 21:54:40 +0800

    Add files via upload

--- s3_ssr.sh
+++ s3_ssr.sh
@@ -6,6 +6,12 @@ green='\033[0;32m'
 yellow='\033[0;33m'
 plain='\033[0m'
 
+get_ip(){
+    local IP=$( ip addr | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | egrep -v "^192\.168|^172\.1[6-9]\.|^172\.2[0-9]\.|^172\.3[0-2]\.|^10\.|^127\.|^255\.|^0\." | head -n 1 )
+    [ -z ${IP} ] && IP=$( wget -qO- -t1 -T2 ipv4.icanhazip.com )
+    [ -z ${IP} ] && IP=$( wget -qO- -t1 -T2 ipinfo.io/ip )
+    echo ${IP}
+}
 
 echo -e "${green} Install SSR ${plain}"
 echo -e "${green} S1 Type: Default as SSR Python ${plain}"
@@ -15,3 +21,6 @@ echo -e "${green} S4 Encryption: Select 7 as aes-256-cfb ${plain}"
 
 cd /root/ssr_result
 ./shadowsocks-all.sh 
+
+echo -e "Your Server IP : ${red} scp root@$(get_ip):/root/ssr_result/shadowsocks_python_qr.png D:\H\Centos7\CentOS7\QRCodes\Vultr_$(get_ip).png ${plain}"
+
