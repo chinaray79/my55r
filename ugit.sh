7ba9a10b061b1469d4f78c9551cc9e112d0d2725
chinaray79 <rayjohnson79@gmail.com>
2019-09-21 13:56:33 +0800

    auto

--- ugit.sh
+++ ugit.sh
@@ -9,17 +9,20 @@ plain='\033[0m'
 if [ -d "/root/.git" ]; then
   cd /root
   git pull
+  chmod 777 *.sh
 else
   if [ -d "/root/my55r"]; then
     cd /root/my55r
 	mv /root/my55r/.git /root/.git
 	cd /root
 	git pull
+	chmod 777 *.sh
   else
     cd /root
 	git clone https://github.com/chinaray79/my55r
 	mv /root/my55r/.git /root/.git
 	cd /root
 	git pull
+	chmod 777 *.sh
   fi
 fi
