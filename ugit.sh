c8356f3dacd36f4e3ee836d3a2c57af6b46901a3
chinaray79 <rayjohnson79@gmail.com>
2019-09-21 14:57:22 +0800

    auto

--- ugit.sh
+++ ugit.sh
@@ -5,15 +5,17 @@ red='\033[0;31m'
 green='\033[0;32m'
 yellow='\033[0;33m'
 plain='\033[0m'
-  if [ -d "/root/my55r" ] ; then
+if [ -d "/root/my55r" ] ; then
 	cd /root/my55r
 	git pull
-  else
+else
 	cd /root
 	git clone https://github.com/chinaray79/my55r
 	\cp -f my55r/ugit.sh ./u.sh
-  fi
-  cd /root
-  \cp -f my55r/*.sh ./
-  cd /root
-  chmod 777 *.sh 
+fi
+cd /root
+\cp -f my55r/*.sh ./
+cd /root
+chmod 777 *.sh 
+
+ #
