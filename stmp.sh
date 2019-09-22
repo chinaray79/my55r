deafbe65274809c9ffe3534fa65df5de803ec7a3
chinaray79 <rayjohnson79@gmail.com>
2019-09-22 15:24:02 +0800

    auto

--- stmp.sh
+++ stmp.sh
@@ -1,23 +1,60 @@
 #!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
-export PATH
 red='\033[0;31m'
 green='\033[0;32m'
 yellow='\033[0;33m'
 plain='\033[0m'
+#/etc/shadowsocks-python/config.json
+ssr_cfig_file="/etc/shadowsocks-python/config.json"
 
-shadowserver="0.0.0.0"
-shadowsocksport=50505
-shadowoldport=14326
-shadowlocalip="127.0.0.1"
-shadowlocalport="1080"
-shadowsockspwd="aatkukb79"
-shadowtimeout=300
-shadowsockscipher="aes-256-cfb"
-fast_open="false"
-
+get_ip(){
+    local IP=$( ip addr | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | egrep -v "^192\.168|^172\.1[6-9]\.|^172\.2[0-9]\.|^172\.3[0-2]\.|^10\.|^127\.|^255\.|^0\." | head -n 1 )
+    [ -z ${IP} ] && IP=$( wget -qO- -t1 -T2 ipv4.icanhazip.com )
+    [ -z ${IP} ] && IP=$( wget -qO- -t1 -T2 ipinfo.io/ip )
+    echo ${IP}
+}
+get_json_value(){
+	if [ $# -eq 2 ] ; then
+		local val=$(cat $1 | grep $2)
+		val=${val#*:}
+		val=${val%*,}
+		val=${val#*\"}
+		val=${val%*\"}
+		echo ${val}
+	fi
+}
+get_ssr_value(){
+	if [ $# -eq 1 ] ; then
+		echo $(get_json_value ${ssr_cfig_file} $1)
+	fi
+}
+get_ssr_parameters(){
+	shadowserver=$(get_ssr_value server\")
+	shadowsocksport=$(get_ssr_value server_port)
+	shadowlocalip=$(get_ssr_value local_address)
+	shadowlocalport=$(get_ssr_value local_port)
+	shadowsockspwd=$(get_ssr_value password)
+	shadowtimeout=$(get_ssr_value timeout)
+	shadowsockscipher=$(get_ssr_value method)
+	fast_open=$(get_ssr_value fast_open)
+	server_infor=$(get_json_value server.json server_infor)
+}
+show_ssr_parameters(){
+	echo ${shadowserver}
+	echo ${shadowsocksport}
+	echo ${shadowlocalip}
+	echo ${shadowlocalport}
+	echo ${shadowsockspwd}
+	echo ${shadowtimeout}
+	echo ${shadowsockscipher}
+	echo ${fast_open}
+	echo ${server_infor}
+}
 qr_generate_python(){
 	cur_dir=$( pwd )
+	local tmp_org="${shadowsockscipher}:${shadowsockspwd}@$(get_ip):${shadowsocksport}"
+	echo "QR Code Source is:"
+	echo -e "ss://${tmp_org}"
     local tmp=$(echo -n "${shadowsockscipher}:${shadowsockspwd}@$(get_ip):${shadowsocksport}" | base64 -w0)
     local qr_code="ss://${tmp}"
     echo
@@ -28,3 +65,20 @@ qr_generate_python(){
     echo "Your QR Code has been saved as a PNG file path:"
     echo -e "${green} ${cur_dir}/shadowsocks_python_qr.png ${plain}"
 }
+cp_funs(){
+	echo ""
+	echo ""
+	echo -e "${red}--------------------------------------------------------------------------"
+	echo ""
+	echo -e "${green}scp root@$(get_ip):/root/ssr_$(get_ip).txt D:\\SSR\\SSR_$(get_ip).txt\r"
+	echo -e "${green}scp root@$(get_ip):/root/shadowsocks_python_qr.png D:\\SSR\\SSR_$(get_ip).png\r"
+	echo -e "${green}echo %date% %time%  The SSR For IP:$(get_ip) Port:${shadowsocksport} Server:${server_infor}>> D:\\SSR\\SSR_Result.txt"
+	echo -e "${green}more D:\\SSR\\ssr_$(get_ip).txt >> D:\\SSR\\SSR_Result.txt${plain}"
+	echo -e "${green}echo. >> D:\\SSR\\SSR_Result.txt${plain}"
+	echo -e "${green}echo. >> D:\\SSR\\SSR_Result.txt${plain}"
+	echo ""
+	echo ""
+	echo ""
+}
+get_ssr_parameters
+show_ssr_parameters
